b0abb48df4e7b9ea159ff4cbf74bb2eb4ccd31e3
johnjohn123456 <johnaf123456@gmail.com>
2019-01-03 12:20:37 +0200

    api

---
+++ server/server.js
@@ -0,0 +1,26 @@
+const { mongoose } = require('./db/db');
+const {User} = require('./models/user');
+const {Todo} = require('./models/todo');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+console.log(mongoose);
+
+const app = express();
+app.use(bodyParser());
+
+app.post('/', (req, res) => {
+  const todo = new Todo({
+    text: req.body.text
+  });
+
+  todo.save().then( (docs) => {
+    res.send(docs);
+  }).catch( (e) => {
+    res.status(400).send(e);
+  })
+});
+
+app.listen(3000, () => {
+  console.log('server is running on port');
+});
