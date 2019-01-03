207071e4694770131f53230eefdcbca8d7e36f6a
johnjohn123456 <johnaf123456@gmail.com>
2019-01-03 14:29:43 +0200

    delete

--- server/server.js
+++ server/server.js
@@ -36,11 +36,27 @@ app.get('/todos/:id', (req, res) => {
     res.status(404).send('Invalid id');
   }
 
-  User.findById(id).then( (user) => {
+  Todo.findById(id).then( (user) => {
     if(!user) {
       res.status(404).send([]);
     }
     res.status(200).send(user);
+  }).catch( (e) => {
+    res.status(400).send([]);
+  })
+})
+
+app.delete('/todos/:id', (req, res) => {
+  let id = req.params.id;
+  if(!ObjectID.isValid(id)) {
+    res.status(404).send('Invalid id');
+  }
+
+  Todo.findOneAndDelete({_id: id}).then( (results) => {
+    if(!res) {
+      return res.status(404).send([]);
+    }
+    return res.send(results);
   }).catch( (e) => {
     res.status(400).send([]);
   })
