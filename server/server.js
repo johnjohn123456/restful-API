a26472a8428723bd22ec6ba4a2e4021871da4cf4
johnjohn123456 <johnaf123456@gmail.com>
2019-01-03 13:44:32 +0200

    byId

--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
 const { mongoose } = require('./db/db');
+const {ObjectID} = require('mongodb');
 const {User} = require('./models/user');
 const {Todo} = require('./models/todo');
 const express = require('express');
 const bodyParser = require('body-parser');
 
-console.log(mongoose);
 
 const app = express();
 app.use(bodyParser());
@@ -28,6 +28,23 @@ app.get('/todos', (req, res) => {
     res.status(400).send(e);
   })
 });
+
+app.get('/todos/:id', (req, res) => {
+  let id = req.params.id;
+
+  if(!ObjectID.isValid(id)) {
+    res.status(404).send('Invalid id');
+  }
+
+  User.findById(id).then( (user) => {
+    if(!user) {
+      res.status(404).send([]);
+    }
+    res.status(200).send(user);
+  }).catch( (e) => {
+    res.status(400).send([]);
+  })
+})
 
 app.listen(3000, () => {
   console.log('server is running on port');
